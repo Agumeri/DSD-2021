3b0cb90865e163e1c9a2d44e2c45b6e26633bbbe
Agustín Mérida Gutiérrez <41384961+Agumeri@users.noreply.github.com>
2021-06-04 13:40:31 +0200

    feat(P4): Finalizada P4

--- servidor.js
+++ servidor.js
@@ -8,6 +8,7 @@ var weather = require("weather-js");
 
 var MongoClient = require('mongodb').MongoClient;
 var MongoServer = require('mongodb').Server;
+const { SSL_OP_COOKIE_EXCHANGE } = require("constants");
 var mimeTypes = { "html": "text/html", "jpeg": "image/jpeg", "jpg": "image/jpeg", "png": "image/png", "js": "text/javascript", "css": "text/css", "swf": "application/x-shockwave-flash"};
 
 // Umbrales de los sensores
@@ -33,29 +34,47 @@ var aire_acondicionado = false;
 //
 
 function agente_sistema(){
-	let t = Date.now()
-
+	var dia = new Date();
+    var t = "[" + dia.getDate() + "/" + dia.getMonth() + " | " + dia.getHours() + ":" + dia.getMinutes() + ":" + dia.getSeconds() + "] --> ";
+	
 	// Administramos temperatura
 	if (parseInt(tmax) >= parseInt(TEMP_MAX)){
 		aire_acondicionado = true;
 		tmax = "27";
-		log.push(t + ": Se ha encendido el aire acondicionado. Que calor hace!!!");
-		log.push("Temperatura actual: 27ºC");
+		tmin = "7";
+
+		let l = t + "Se ha encendido el aire acondicionado. Que calor hace!!!. La temperatura maxima pasa a ser de 27ºC y la minima 7ºC";
+		log.push(l);
+		console.log(l);
+
 	} else if(parseInt(tmin) <= parseInt(TEMP_MIN)){
 		aire_acondicionado = false;
-		tmin = "5";
-		log.push(t + ": Apagando el aire acondicionado, que hace fresquete");
+		tmax = "30";
+		tmin = "10";
+
+
+		let l = t + "Apagando el aire acondicionado, que hace fresquete. La temperatura maxima pasa a ser de 30ºC y minima 10ºC ";
+		log.push(l);
+		console.log(l);
 	}
 
 	// Administramos luminosidad
 	if (parseInt(lmax) >= parseInt(LUM_MAX)){
 		persiana = false;
 		lmax = "80";
-		log.push(t + ": Demasiada luz, cerrando la persiana");
+		lmin = "50";
+
+		let l = t + "Demasiada luz, cerrando la persiana. La luminosidad maxima es ahora del 80% y la minima del 50%;";
+		log.push(l);
+		console.log(l);
 	} else if(parseInt(lmin) <= parseInt(LUM_MIN)){
 		persiana = true;
+		lmax = "50";
 		lmin = "20"
-		log.push(t + ": Abriendo la persiana, que esto parece una cueva");
+
+		let l = t + "Abriendo las persianas, que esto parece una cueva. La luminosidad maxima es ahora del 50% y la minima del 20%";
+		log.push(l);
+		console.log(l);
 	}
 }
 
@@ -97,24 +116,46 @@ var httpServer = http.createServer(
 httpServer.listen(8000);
 var io = socketio(httpServer);
 
+var prediccionTiempo = [];
+var semana = [];
+
 weather.find(
 	{
-		search: 'Priego de Córdoba, España',
+		search: 'Granada, España',
 		degreeType: 'C'
 	}, 
 	function(err,result){
 		if(err) console.log(err);
 		// console.log(JSON.stringify(result,null,2));
-		console.log(result);
+		prediccionTiempo = result[0].forecast;
+		for(let i = 0; i<prediccionTiempo.length; i++){
+			semana.push(prediccionTiempo[i]);
+		}
+		// console.log(semana);
+		// console.log(semana[0].date.split('-').reverse().join('-'));
+		
+		// console.log(result);
+
 	}
 );
 
-MongoClient.connect("mongodb://localhost:27017/", {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
+// Se pueden seleccionar dos direcciones para la base de datos
+// COMENTAR LA QUE NO SE VAYA A UTILIZAR
+
+// - Si queremos usar una BD local:
+var bd_name = "mongodb://localhost:27017/";
+
+// - Si queremos usar la BD local proporcionada por un cluster gratuito:
+// var bd_name = "mongodb+srv://usuariop4:holahola@cluster0.9ngjj.mongodb.net/pruebaBaseDatos?retryWrites=true&w=majority";
+
+
+
+MongoClient.connect(bd_name, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
 	var dbo = db.db("pruebaBaseDatos");
+	// var dbo = db.db("pruebaBaseDatos");
 	var datos = [];
 	
 	// filtro pa ver si existe la BD 
-	
 	//
 
 	dbo.collection("datos_sensores", function(err, collection){
@@ -141,21 +182,23 @@ MongoClient.connect("mongodb://localhost:27017/", {useNewUrlParser: true, useUni
 			// 	firstTime = false;
 			// }
 			io.emit('obtener',datos);
+			io.emit('weather',semana);
 			
 			// Actualizar valores de temperatura
 			client.on('nueva_temperatura', function(data){
 				tmax =data.tmax;
 				tmin =data.tmin;
 
-				// collection.updateOne({"Tipo": "temperatura"},
-				// {
-				// 	$set: {
-				// 			'tmin': data.tmin,
-				// 			'tmax': data.tmax
-				// 		  }
-				// });
-				let t = Date.now()
-				log.push(t + ": Se ha actualizado la temperatura a " + tmax + "ºC");
+				if(parseInt(tmax) < parseInt(tmin)){
+					tmin = tmax - 20;
+				}
+
+
+				var dia = new Date();
+    			var t = "[" + dia.getDate() + "/" + dia.getMonth() + " | " + dia.getHours() + ":" + dia.getMinutes() + ":" + dia.getSeconds() + "] --> ";
+				let l = t + "Registrando nuevas temperaturas: Tmax:" + tmax + "ºC, Tmin:" + tmin + "ºC";
+				log.push(l);
+				console.log(l);
 				
 				actualizar();
 			});
@@ -165,15 +208,15 @@ MongoClient.connect("mongodb://localhost:27017/", {useNewUrlParser: true, useUni
 				lmax =data.lmax;
 				lmin =data.lmin;
 
-				// collection.updateOne({"Tipo": "luminosidad"},
-				// {
-				// 	$set: {
-				// 			'lmin': data.lmin,
-				// 			'lmax': data.lmax
-				// 		  }
-				// });
-				let t = Date.now()
-				log.push(t + ": Se ha actualizado la luminosidad a " + lmax + "%");
+				if(parseInt(lmax) < parseInt(lmin)){
+					lmin = lmax - 20;
+				}
+
+				var dia = new Date();
+    			var t = "[" + dia.getDate() + "/" + dia.getMonth() + " | " + dia.getHours() + ":" + dia.getMinutes() + ":" + dia.getSeconds() + "] --> ";
+				let l = t + "Registrando nuevas luminosidad: Lmax:" + lmax + "%, Lmin:" + lmin + "%";
+				log.push(l);
+				console.log(l);
 				
 				actualizar();
 			});
@@ -183,27 +226,26 @@ MongoClient.connect("mongodb://localhost:27017/", {useNewUrlParser: true, useUni
 				persiana = data.persiana;
 				aire_acondicionado = data.aire;
 
-				// collection.updateOne({"Tipo": "actuadores"},
-				// {
-				// 	$set: {
-				// 			'persiana': persiana,
-				// 			'aire_acondicionado': aire_acondicionado
-				// 		  }
-				// });
 				actualizar();
-				let t = Date.now()
-				log.push(t + ": Se ha actualizado el estado de los actuadores");
+				var dia = new Date();
+    			var t = "[" + dia.getDate() + "/" + dia.getMonth() + " | " + dia.getHours() + ":" + dia.getMinutes() + ":" + dia.getSeconds() + "] --> ";
+				let l = t + "Actualizando estado de los actuadores: Aire Acondicionado: " + aire_acondicionado + " | Persiana: " + persiana;
+				// log.push(l);
+				console.log(l);
 			});
 
-			client.on('log',function(logs){
+			client.on('log',function(any){
 				var data = log;
 				io.emit('log',data);
 			})
 
+			client.on('weather',function(any){
+				var week = semana;
+				io.emit('weather',week);
+			})
+
 			// Recoger datos de la BD
 			function recogerDatos(){
-				var res = false;; 
-
 				collection.find().toArray(function(err, results){
 					// Asignamos datos
 					datos = results;
@@ -231,8 +273,10 @@ MongoClient.connect("mongodb://localhost:27017/", {useNewUrlParser: true, useUni
 				asignarVariables();
 				recogerDatos();
 
+				// client broadcast emit
 				io.emit('obtener', datos);				
 				io.emit('log',log);
+				io.emit('weather',semana);
 			}
 
 			// Actualizamos en la base de datos con todos los valores actualizados
@@ -299,5 +343,4 @@ console.log("Servicio MongoDB iniciado");
 console.log("Servidor escuchando en el puerto 8000");
 console.log("\nRutas disponibles")
 console.log("Sensor --> localhost:8000/sensor");
-console.log("Usuario --> localhost:8000/usuario");
-
+console.log("Usuario --> localhost:8000/usuario\n");
